6162b91979d952c76e8a51273477e733e2387e94
unknown <ricardosard@hotmail.com>
2022-03-28 11:42:41 -0400

    Codigo inicial

---
+++ src/pages/Home/Index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.css';
+import Header from "./components/Header";
+
+function Home(){
+    return (
+    <div>
+        <Header/>
+            <main className="main">
+                <div className='navbar'>
+         
+                </div>
+                
+            </main>
+       
+    </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className="header">
+            <div className="toolbar">
+                <div >
+                <span>home butoes</span>
+                </div>
+
+                <div >
+
+                <span>nome da barra</span>
+                <span>barra de pesquisa</span>
+                <button>pesquisa</button>
+        
+                </div>
+            </div>
+            </header>
+    );
+}
+
+export default Header;
